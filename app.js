20e5c162971a48210306d8cf06f3aea6b2e70a07
unknown <mriduldey94@gmail.com>
2021-10-10 04:43:29 +0530

    1.basic ejs template created 2. app js updated

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-const express = require("express");
+const express = require('express');
+const { nanoid } = require('nanoid');
+const createHttpError = require('http-errors');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// Local imports
 
 const app = express();
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res, next) => {
+  res.render('index');
+});
+
+app.post('/', async (req, res, next) => {
+  res.send('url posted');
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
