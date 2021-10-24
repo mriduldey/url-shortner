17287beb7f48549972f6e83eed2555d0789b3ad6
unknown <mriduldey94@gmail.com>
2021-10-25 01:19:18 +0530

    1. db connection added and tested, 2. url shortner post function added 3. short url redirection added 4. ejs templating for error and short url fixed

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const { nanoid } = require('nanoid');
 const createHttpError = require('http-errors');
 const mongoose = require('mongoose');
 const path = require('path');
+const { env } = process;
 
 // Local imports
+const ShortUrl = require('./models/url.model');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const uri = `mongodb+srv://${env.username}:${env.password}@urlshortener.efmey.mongodb.net/${env.database}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.log(err.message));
 
 app.set('view engine', 'ejs');
 
@@ -18,7 +30,46 @@ app.get('/', async (req, res, next) => {
 });
 
 app.post('/', async (req, res, next) => {
-  res.send('url posted');
+  try {
+    const { url } = req.body;
+    if (!url) {
+      console.log('no url');
+      throw createHttpError.BadRequest('Provide a valid url');
+    }
+    const urlExists = await ShortUrl.findOne({ url });
+    if (urlExists) {
+      res.render('index', { shortUrl: `http://localhost:3000/${urlExists.shortId}` });
+      return;
+    }
+
+    const shortUrl = new ShortUrl({ url, shortId: nanoid(12) });
+    const result = await shortUrl.save();
+    res.render('index', { shortUrl: `http://localhost:3000/${urlExists.shortId}` });
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.get('/:shortId', async (req, res, next) => {
+  try {
+    const { shortId } = req.params;
+    const result = await ShortUrl.findOne({ shortId });
+    if (!result) {
+      throw createHttpError.NotFound('Short URL does not exist');
+    }
+    res.redirect(result.url);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use((req, res, next) => next(createHttpError.NotFound()));
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('index', { error: err.message });
+});
+
+const PORT = env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
